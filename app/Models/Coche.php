af7be2bbcbd8ee846fa291ac3f0feb561daa3c8a
Juanfranciscomp30 <juanfranciscomoralesplaza30@gmail.com>
2025-02-10 16:16:03 +0100

    Actualizamos Scoope

--- app/Models/Coche.php
+++ app/Models/Coche.php
@@ -14,4 +14,24 @@ class Coche extends Model
         'color',
         'precio'
     ];
+
+    /* Scope para filtrar por marca */
+    public function scopeMarcaScope($query, $marca)
+    {
+        return $query->where('marca', $marca);
+    }
+     
+     /* Scope para filtrar por color */
+    public function scopeColorScope($query, $color)
+    {
+        return $query->where('color', $color);
+    }
+
+    
+     /*Scope para filtrar por precio */
+     
+    public function scopePrecioScope($query, $precio)
+    {
+        return $query->where('precio', '<=', $precio);
+    }
 }
